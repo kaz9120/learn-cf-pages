6f4def72ee3b9e1edae7f2351d28f4ed3e2f561a
yamamoto_kazumasa <yamamoto_kazumasa@uniquevision.co.jp>
2024-08-13 21:28:32 +0900

    エラーを修正

--- functions/api/hello.ts
+++ functions/api/hello.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 
 interface Env {}
 
-export const onRequest: PagesFunction<Env> = async (context) => {
+export const onRequest: PagesFunction<Env> = async () => {
     const id: string = uuid();
     return new Response(`Hello, World!! ${id}`);
 }
